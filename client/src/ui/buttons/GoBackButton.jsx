d06b9d7ca77617742894ea9675be16542d813da6
Serhii <propolovchesnochok@gmail.com>
2023-12-03 22:02:43 +0200

    add go back button

---
+++ client/src/ui/buttons/GoBackButton.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import Left from '../../assets/svg/angle-left.svg';
+
+export const GoBackButton = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <button onClick={() => navigate(-1)} className="absolute flex flex-row items-center top-10 left-10 text-center shadow-lg rounded-lg text-lg px-5 py-1 hover:bg-gray-100 font-medium">
+            <img className="w-12" src={Left} alt="" />
+            Повернутися назад
+        </button>
+    )
+}
