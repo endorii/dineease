df7405fc50c9daba44868a447db85e3194962bc2
Serhii <propolovchesnochok@gmail.com>
2023-12-03 21:54:20 +0200

    Minor changes and improvements

--- client/src/actions/employees.actions.js
+++ client/src/actions/employees.actions.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { setEmployeesByRestaurant } from "../store/slices/employees.slice";
 
 export const getEmployees = async () => {
     try {
@@ -8,3 +9,17 @@ export const getEmployees = async () => {
         console.log(e.response.data.message);
     }   
 }
+
+// export const getEmployeesByRestaurant = (restaurantName) => {
+//     return async (dispatch) => {
+//         try {
+//             const response = await axios.get("http://localhost:3001/employees");
+//             const employees = response.data;
+//             const filteredEmployees = employees.filter(employee => employee.restaurant === restaurantName);
+//             dispatch(setEmployeesByRestaurant(filteredEmployees))
+//             return filteredEmployees;
+//         } catch (e) {
+//             console.log(e.response.data.message);
+//         }   
+//     }
+// }
