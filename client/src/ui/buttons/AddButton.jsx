ce9c20239afd2213de050141245d348bc2d51dc7
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-06 14:16:14 +0200

    Added AddButton

---
+++ client/src/ui/buttons/AddButton.jsx
@@ -0,0 +1,11 @@
+import Plus from '../../assets/svg/plus.svg' ;
+
+export const AddButton = ({customFunction}) => {
+    return (
+        <button className="flex items-center bg-teal-600 hover:bg-teal-700 rounded-lg px-7 py-2 text-white font-medium drop-shadow-md"
+            onClick={() => customFunction(true)}
+        >Додати
+            <img className='w-7 inline pl-2' src={Plus} alt="" />
+        </button>
+    )
+}
