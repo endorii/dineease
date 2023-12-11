f94c1965abc120824655ec166f089ea639fe9063
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-11 22:26:16 +0200

    Waiter account changes

--- client/src/components/CurrentEmployeeAccount.jsx
+++ client/src/components/CurrentEmployeeAccount.jsx
@@ -3,13 +3,15 @@ import { useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { getEmployeeByRestaurantAndPin } from "../actions/employees.actions"
+import Logout from '../assets/svg/logout.svg'
+import Send from '../assets/svg/send.svg'
 // import { addNeed } from "./needs.actions"
 // import { addContactText } from "./contactText.actions"
 
 export const CurrentEmployeeAccount = () => {
 
-    const { employees } = useSelector(state => state.employees)
-    const startTime = new Date('Fri Oct 24 2023 10:30:57 GMT+0300');
+    const { employees } = useSelector(state => state.employees);
+    const startTime = new Date('Sun Dec 10 2023 20:30:57 GMT+0300');
     const navigate = useNavigate();
 
     const [currentTime, setCurrentTime] = useState('');
@@ -24,10 +26,10 @@ export const CurrentEmployeeAccount = () => {
         const now = new Date();
         const diffInMilliseconds = now - startTime;
         const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
-        const seconds = diffInSeconds % 60;
+        // const seconds = diffInSeconds % 60;
         const minutes = Math.floor(diffInSeconds / 60) % 60;
         const hours = Math.floor(diffInSeconds / 3600);
-        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        return `${hours.toString().padStart(2, '0')}год. ${minutes.toString().padStart(2, '0')}хв.`;
     };
 
     const now = new Date().toLocaleString();
@@ -38,88 +40,64 @@ export const CurrentEmployeeAccount = () => {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setCurrentTime(getCurrentOnlineTime(startTime))
+            setCurrentTime(getCurrentOnlineTime(startTime));
         }, 1000);
         return () => clearInterval(timer);
     }, []);
 
     return (
-        <div className="h-screen overflow-y-scroll">
-            <div className="py-10">
-                <div className="flex text-center justify-center gap-10 flex-wrap">
-                    <div className="">
-                        <div className="py-10 rounded-lg shadow-md bg-white w-auto top-[25%] left-[17%] flex flex-col fustify-center items-center text-4xl font-thin">
-                            Час вашого сеансу:
-                            <div className="text-9xl text-gray-700 font-bold p-10">
-                                {currentTime}
-                            </div>
-                            <div>
-                                <button className="px-6 py-3 bg-red-500 text-3xl text-white rounded-lg" onClick={async () => { navigate('/employees'); }}>Завершити робочу зміну</button>
-                            </div>
+        <div className="bg-sky-50 overflow-y-scroll h-[93vh]">
+            <div className="p-10 flex justify-center gap-7 ">
+
+                <div className="flex flex-col gap-5 w-[47%]">
+
+                    <div className="bg-white border shadow-inner p-10 flex flex-col gap-5 rounded-lg">
+                        <div className="text-3xl text-sky-900 font-medium text-center">Час вашого сеансу:</div>
+                        <div className="text-center text-sky-900 text-8xl font-bold">{currentTime}</div>
+                        <div className="w-full text-center mt-4">
+                            <button className="bg-yellow-600 hover:bg-yellow-700 text-white px-5 py-3 text-lg rounded-md m">
+                                Закінчити робочу зміну
+                                <img className="w-6 inline-block ml-3 mb-1" src={Logout} alt="" />
+                            </button>
                         </div>
                     </div>
-                    <div className="flex gap-10 w-[43%] flex-col">
-                        <div className=" bg-white rounded-xl shadow-md">
-                            <p className="text-2xl p-5 font-medium text-gray-700">Загальна інформація:</p>
-                            <ul className="flex flex-col gap-10 p-10 text-left text-xl font-thin">
-                                <li>
-                                    <p>
-                                        Імя:
-                                        {/* {employees[0].name} */}
-                                    </p>
-                                </li>
-                                <li>
-                                    <p>
-                                        Посада:
-                                        {/* {employees[0].position} */}
-                                    </p>
-                                </li>
-                                <li>
-                                    <p>
-                                        Робочі години за весь час:
-                                        {/* {employees[0].totalWorkingTime} */}
-                                    </p>
-                                </li>
-                                <li>
-                                    <p>
-                                        Кількість обслужених замовлень\столиків: 100
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className=" bg-white rounded-xl shadow-md">
-                        <p className="text-2xl p-5 font-medium text-gray-700">Інформація за зміну:</p>
-                        <ul className="flex flex-col gap-10 p-10 text-left text-xl font-thin">
-                            <li>
-                                <p>
-                                    Початок робочої зміни о: {startTime.toLocaleTimeString()}
-                                </p>
-                            </li>
-                            <li>
-                                <p>
-                                    Кількість обслужених замовлень\столиків за зміну: 100
-                                </p>
-                            </li>
+
+                    <div className=" flex flex-col gap-6 bg-white border shadow-inner p-10 rounded-lg">
+                        <div className="text-3xl text-sky-900 font-medium text-center">Інформація за зміну:</div>
+                        <ul className="text-xl text-sky-800">
+                            <li>Кількість обслужених столиків: <span className="text-xl underline font-medium text-sky-950">10</span></li>
+                            <li>Початок робочої зміни о: <span className="text-xl underline font-medium text-sky-950">20:30:57</span></li>
                         </ul>
                     </div>
-                    <div className=" bg-white rounded-xl shadow-md">
-                        <p className="text-2xl p-5 font-medium text-gray-700">Побажання та потреби:</p>
-                        <div className="px-10">
-                            <textarea onChange={(e) => { setWishesAreaText(e.target.value) }} value={wishesAreaText} placeholder="Введіть повідомлення для адміністратора..." className="rounded-lg bg-gray-100 border-b-4 px-5 py-3 w-full text-lg" name="" id="" cols="30" rows="10"></textarea>
+
+                    <div className="flex flex-col gap-6 bg-white border shadow-inner p-10 rounded-lg">
+                        <div className="text-3xl text-sky-900 font-medium text-center">Загальна інформація:</div>
+                        <ul className="text-xl text-sky-800">
+                            <li>Ім'я: <span className="text-xl underline font-medium text-sky-950">Користувач</span></li>
+                            <li>Посада: <span className="text-xl underline font-medium text-sky-950">Офіціант</span></li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="flex flex-col gap-5 w-[47%]">
+                    <div className="flex flex-col gap-4 bg-white border shadow-inner p-10 rounded-lg">
+                        <div className="text-3xl text-sky-900 font-medium text-center">Побажання та потреби:</div>
+                        <div className="flex border-2 border-sky-900 rounded-xl p-1 mt-2">
+                            <textarea onChange={(e) => { setWishesAreaText(e.target.value) }} value={wishesAreaText} placeholder="Введіть повідомлення для адміністратора..." className="w-full text-lg p-5 outline-none text-sky-900 font-medium" name="" id="" rows="7"></textarea>
                             <button onClick={() => {
-                                // addNeed(wishesAreaText, currentEmployee.name, now); setWishesAreaText('')
-                            }} className="bg-green-500 hover:bg-green-600 rounded-lg m-5 px-7 py-2 text-white font-medium drop-shadow-md disabled:bg-green-900/20 disabled:hover:bg-green-900/20 disabled:text-gray-100 disabled:cursor-not-allowed">Відправити</button>
+                                // addNeed(wishesAreaText, currentEmployee.name, now); 
+                                setWishesAreaText('');
+                            }} className="p-6 bg-sky-800 hover:bg-sky-900 active:bg-sky-950 rounded-md "><img className="w-10" src={Send} alt="" /></button>
                         </div>
-                        <div className=" bg-white rounded-xl shadow-md">
-                            <p className="text-2xl p-5 font-medium text-gray-700">Зворотній зв'язок:</p>
-                            <div className="px-10">
-                                <textarea onChange={(e) => { setContactAreaText(e.target.value) }} value={contactAreaText} placeholder="Введіть повідомлення для адміністратора..." className="rounded-lg bg-gray-100 border-b-4 px-5 py-3 w-full text-lg" name="" id="" cols="30" rows="10"></textarea>
-                                <button onClick={() => {
-                                    // addContactText(contactAreaText, currentEmployee.name, now); 
-                                    setContactAreaText('')
-                                }} className="bg-green-500 hover:bg-green-600 rounded-lg m-5 px-7 py-2 text-white font-medium drop-shadow-md disabled:bg-green-900/20 disabled:hover:bg-green-900/20 disabled:text-gray-100 disabled:cursor-not-allowed">Відправити</button>
-                            </div>
+                    </div>
+                    <div className="flex flex-col gap-4 bg-white border shadow-inner p-10 rounded-lg">
+                        <div className="text-3xl text-sky-900 font-medium text-center">Зворотній зв'язок:</div>
+                        <div className="flex border-2 border-sky-900 rounded-xl p-1 mt-2">
+                            <textarea onChange={(e) => { setContactAreaText(e.target.value) }} value={contactAreaText} placeholder="Введіть повідомлення для адміністратора..." className="w-full text-lg p-5 outline-none text-sky-900 font-medium" name="" id="" rows="7"></textarea>
+                            <button onClick={() => {
+                                // addContactText(contactAreaText, currentEmployee.name, now); 
+                                setContactAreaText('');
+                            }} className="p-6 bg-sky-800 hover:bg-sky-900 active:bg-sky-950 rounded-md "><img className="w-10" src={Send} alt="" /></button>
                         </div>
                     </div>
                 </div>
