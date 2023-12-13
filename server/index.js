21ba0cad070499ed90d55fb25ae9670dc578f03e
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-13 21:08:42 +0200

    Minor changes and improvements

--- server/index.js
+++ server/index.js
@@ -3,10 +3,17 @@ const mongoose = require('mongoose');
 const config = require('config');
 const app = express();
 const PORT = config.get('PORT')
-// const cors = require('cors');
+const cors = require('cors');
+const authRouter = require('../server/routes/auth.routes');
+const restaurantsRouter = require('../server/routes/restaurants.routes');
+const employeesRoutes = require('../server/routes/employees.routes');
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/", restaurantsRouter);
+app.use("/api/", employeesRoutes);
 
 const start = async () => {
     try {
