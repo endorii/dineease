af1835b2f8d5a6e4f03d22e60a040527af719b20
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-20 18:36:31 +0200

    Added routes for employee (POST, PUT, DELETE)

--- server/routes/employees.routes.js
+++ server/routes/employees.routes.js
@@ -1,6 +1,6 @@
 const Router = require('express');
 
-// const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 const Employee = require('../models/Employee');
 
 const router = new Router();
@@ -8,22 +8,91 @@ const router = new Router();
 router.get('/employees',
     async (req, res) => {
         try {
-            const {restaurantName} = req.query;
+            const { restaurantName } = req.query;
 
             let employees;
             if (restaurantName) {
-                employees = await Employee.find({restaurant: restaurantName});
+                employees = await Employee.find({ restaurant: restaurantName });
             } else {
                 employees = await Employee.find({});
             }
 
-            return res.json({employees});
+            return res.json({ employees });
 
         } catch (e) {
             console.log(e);
-            res.send({message: "Server error"});
+            res.send({ message: "Server error" });
         }
     }
 );
+
+router.post('/employees', authMiddleware,
+    async (req, res) => {
+        try {
+
+            const { name, age, restaurantName, experience, position, salary, password, email, pin } = req.body;
+
+            const candidate = await Employee.findOne({
+                user: req.user.id,
+                $or: [{ pin: pin }, { email: email }]
+            });
+
+            if (candidate) {
+                return res.status(400).json({ message: `Користувач пін-кодом ${pin} або логіном ${login} вже існує` })
+            }
+
+            const employee = new Employee({ user: req.user.id, name, age, restaurant: restaurantName, experience, position, salary, password, email, pin});
+
+            await employee.save();
+
+            return res.json({ message: 'Працівника було додано' });
+
+        } catch (e) {
+            console.log(e);
+            res.send({ message: "Помилка сервера" })
+        }
+    }
+);
+
+router.put('/employees/:_id', authMiddleware,
+    async (req, res) => {
+        try {
+            const { _id } = req.params;
+            const { name, age, restaurantName, experience, position, salary, password, email, pin  } = req.body;
+
+            const updatedEmployee = await Employee.findOneAndUpdate(
+                { _id },
+                { user: req.user.id, name, age, restaurantName, experience, position, salary, password, email, pin },
+                { new: true }
+            );
+
+            if (!updatedEmployee) {
+                return res.status(404).json({ message: `Employee with id ${_id} not found` });
+            }
+
+            return res.json({ message: "Employee was updated" });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: "Server error" });
+        }
+    });
+
+router.delete('/employees/:_id', authMiddleware,
+    async (req, res) => {
+        try {
+            const { _id } = req.params;
+
+            const updatedEmployee = await Employee.findOneAndDelete({ _id });
+
+            if (!updatedEmployee) {
+                return res.status(404).json({ message: `Employee with id ${_id} not found` });
+            }
+
+            return res.json({ message: "Employee was deleted" });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: "Server error" });
+        }
+    });
 
 module.exports = router;
