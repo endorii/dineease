e4ec4394d1078df8fceefb70beac46a60c6749ce
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-20 18:35:44 +0200

    Added actions for employee (Add, edit, delete)

--- client/src/actions/employees.actions.js
+++ client/src/actions/employees.actions.js
@@ -7,19 +7,50 @@ export const getEmployees = async () => {
         return response.data
     } catch (e) {
         console.log(e.response.data.message);
-    }   
+    }
 }
+
+export const addEmployee = async (name, age, restaurantName, experience, position, salary, password, email, pin) => {
+    try {
+        const response = await axios.post("http://localhost:5000/api/employees", { name, age, restaurantName, experience, position, salary, password, email, pin }, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+
+        console.log(response.data.message);
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
+};
 
 export const getEmployeesByRestaurant = (restaurantName) => {
     return async (dispatch) => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/employees?restaurantName=${restaurantName}`);
+            const response = await axios.get(`http://localhost:5000/api/employees?restaurantName=${restaurantName}`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
             const employees = response.data.employees;
-            
+
             dispatch(setEmployeesByRestaurant(employees))
             return employees;
         } catch (e) {
             console.log(e.response.data.message);
-        }   
+        }
+    }
+}
+
+export const editEmployee = async (_id, name, age, restaurantName, experience, position, salary, password, email, pin) => {
+    try {
+        const response = await axios.put(`http://localhost:5000/api/employees/${_id}`, { _id, name, age, restaurantName, experience, position, salary, password, email, pin }, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+        console.log(response.data.message);
+        return response.data
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
+}
+
+
+export const deleteEmployee = async (_id) => {
+    try {
+        const response = await axios.delete(`http://localhost:5000/api/employees/${_id}`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+        console.log(response.data.message);
+        return response.data
+    } catch (e) {
+        console.log(e.response.data.message);
     }
 }
