d019b747d01a781dcc0bdb7a48927e40f2953460
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-23 20:56:56 +0200

    Restaurant changes

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -1,39 +1,12 @@
 const { Schema, model } = require('mongoose');
 
-const MenuItem = new Schema({
-    category: String,
-    items: [{
-        name: String,
-        price: Number,
-        ingredients: [String],
-        calories: String,
-        weight: String,
-        readyTime: String
-    }]
-});
-
-const Need = new Schema({
-    waiterName: String,
-    message: String,
-    time: String,
-    date: String,
-    priority: String
-});
-
-const Feedback = new Schema({
-    waiterName: String,
-    message: String,
-    time: String,
-    date: String
-});
-
 const Restaurant = new Schema({
     name: {type: String, required: true},
     address: String,
     isOpen: Boolean,
-    menu: [MenuItem],
-    needs: [Need],
-    feedback: [Feedback],
+    menu: [{ type: Schema.Types.ObjectId, ref: 'Menu' }],
+    needs: [{ type: Schema.Types.ObjectId, ref: 'Need' }],
+    feedback: [{ type: Schema.Types.ObjectId, ref: 'Feedback' }],
     orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }]
 });
 
--- server/routes/restaurants.routes.js
+++ server/routes/restaurants.routes.js
@@ -1,6 +1,4 @@
 const Router = require('express');
-
-// const authMiddleware = require('../middlewares/auth.middleware');
 const Restaurant = require('../models/Restaurant');
 
 const router = new Router();
@@ -19,49 +17,5 @@ router.get('/restaurants',
     }
 );
 
-router.post('/restaurants/menu',
-    async (req, res) => {
-        try {
-            const {restaurantName} = req.body;
-            const restaurant = await Restaurant.find({name: restaurantName});
-            const menu = restaurant[0].menu;
-            return res.json({menu});
-
-        } catch (e) {
-            console.log(e);
-            res.send({message: "Server error"});
-        }
-    }
-);
-
-router.post('/restaurants/needs',
-    async (req, res) => {
-        try {
-            const {restaurantName} = req.body;
-            const restaurant = await Restaurant.find({name: restaurantName});
-            const needs = restaurant[0].needs;
-            return res.json({needs});
-
-        } catch (e) {
-            console.log(e);
-            res.send({message: "Server error"});
-        }
-    }
-);
-
-router.post('/restaurants/feedback',
-    async (req, res) => {
-        try {
-            const {restaurantName} = req.body;
-            const restaurant = await Restaurant.find({name: restaurantName});
-            const feedback = restaurant[0].feedback;
-            return res.json({feedback});
-
-        } catch (e) {
-            console.log(e);
-            res.send({message: "Server error"});
-        }
-    }
-);
 
 module.exports = router;
