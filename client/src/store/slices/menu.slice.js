466750bda860ba05bc9db8a1a4c2ae1b23a5cec6
Serhii Propolov <propolovchesnochok@gmail.com>
2023-12-23 20:59:13 +0200

    Menu changes

--- client/src/store/slices/menu.slice.js
+++ client/src/store/slices/menu.slice.js
@@ -2,15 +2,15 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getMenuByRestaurant } from '../../actions/menu.actions';
 
 const initialState = {
-    menu: {},
+    menu: [],
     isLoading: false,
     error: null
 }
 
 export const fetchMenu = createAsyncThunk(
     "menu/fetchMenu",
-    async (restaurantName) => {
-        const response = getMenuByRestaurant(restaurantName);
+    async (restaurantId) => {
+        const response = getMenuByRestaurant(restaurantId);
         return response;
     }
 )
