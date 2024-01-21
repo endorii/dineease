1e29e14be76ccd59e91704b968bd7e99afdaa3f9
Serhii Propolov <propolovchesnochok@gmail.com>
2024-01-21 20:34:05 +0200

    Added menu actions

--- client/src/actions/menu.actions.js
+++ client/src/actions/menu.actions.js
@@ -1,12 +1,38 @@
 import axios from "axios"
-// import { setEmployeesByRestaurant } from "../store/slices/employees.slice";
 
-export const getMenuByRestaurant = async (restaurantId) => {
+export const getMenuCategoriesByRestaurant = async (restaurantId) => {
     try {
-        const response = await axios.get(`http://localhost:5000/api/menu/${restaurantId}`);
+        const response = await axios.get(`http://localhost:5000/api/menuCategories/${restaurantId}`);
         return response.data.menu
     } catch (e) {
         console.log(e.response.data.message);
-    }   
+    }
+}
+
+export const addMenuCategory = async (restaurantId, category, logoPath) => {
+    try {
+        const response = await axios.post(`http://localhost:5000/api/menuCategories/${restaurantId}`, { category, logoPath});
+        return response.data.menu
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
+}
+
+export const addMenuItemToCategory = async (restaurantId, categoryId, dishName, dishPrice, dishTime, dishAmount, dishWeight, dishCategory, dishIngredients, dishLogo) => {
+    try {
+        const response = await axios.post(`http://localhost:5000/api/menuDishes/${restaurantId}/${categoryId}`, { dishName, dishPrice, dishTime, dishAmount, dishWeight, dishCategory, dishIngredients, dishLogo });
+        return response.data.menu
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
+}
+
+export const getMenuDishes = async (restaurantId) => {
+    try {
+        const response = await axios.get(`http://localhost:5000/api/menuDishes/${restaurantId}`);
+        return response.data.menu
+    } catch (e) {
+        console.log(e.response.data.message);
+    }
 }
 
